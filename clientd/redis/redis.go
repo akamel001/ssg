97a09d2a39aa88d8a05080f028027071a2aedc77
aimanparvaiz <aimanparvaiz@gmail.com>
2014-12-31 17:04:54 -0800

    Reverting changes to data_dplit not returning anything

--- clientd/redis/redis.go
+++ clientd/redis/redis.go
@@ -32,30 +32,27 @@ func Ssg_redis() {
 	// Random thought; should the metric to be monitored be rad from the config file; metric:file_path
 	for key := range stats_read {
 		info, err := client.Info(stats_read[key])
-		log.Println(&mmm)
 
 		if err !=nil {
 			log.Fatalf("Failed to get data from Redis")
 
 			// if info in not 0, start parsing
 		}else {
-			mmm = data_split(stats_read[key], info, mmm)
-			fmt.Println(mmm)
+			data_split(stats_read[key], info, mmm)
 
 		}
 	}
-	//for key, value := range mmm {
-	//	fmt.Println("Key:", key, "Value:", value)
-	//}
+	for key, value := range mmm {
+		fmt.Println("Key:", key, "Value:", value)
+	}
 
 }
 
-func data_split(delimiter, info string, mmm map[string]string) map[string]string{
+func data_split(delimiter, info string, mmm map[string]string){
 	delimiter = "#" + " " + delimiter
 	result := strings.Split(info, delimiter)
 	k, v := result[0], result[1]
 	mmm[k]=v
-	return mmm
 
 }
 
